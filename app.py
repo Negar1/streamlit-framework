e8ccd2ae9159d03ec0871b5781049c0bcd0dc8cd
Negar1 <40975200+Negar1@users.noreply.github.com>
2021-04-06 19:02:18 -0700

    x-axis type corrected

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ print (df.head(5))
 print (len(df))
 df['time']= pd.to_datetime(df['time'])
 df['date'] = df['time'].dt.date
+df['norm_v']=df['volume']/df['volume'].mean()
 #df['date'] = df['time'].dt.strftime('%m-%d')
 #df['date']=pd.to_datetime(df['date'])
 
@@ -36,7 +37,10 @@ yh=df["high"]
 yl=df["low"]
 yc=df["close"]
 yv=df["volume"]
-p = figure(title="IBM plot, 2021, last 30 days", x_axis_label='date', y_axis_label='IBM')
+ynv=df["norm_v"]
+p = figure(title="IBM plot, 2021, last 30 days", x_axis_type='datetime',
+           x_axis_label='date', y_axis_label='IBM')
+#Open=True
 if Open:
     p.line(x, yo, legend_label="IBM: Open", color='blue')
 if High:
@@ -46,7 +50,7 @@ if Low:
 if Close:
     p.line(x, yc, legend_label='IBM: Close', color='purple')
 if Volume:
-    p.line(x, yv, legend_label='Volume', color='brown')
+    p.line(x, ynv, legend_label='Volume', color='brown')
 
 #p.multi_line([x,x,x], [yo,yh,yl], legend_label="IBM: Open",#, "IBM: high","IBM: Low"], 
 #             color=["green", "blue", "black"], alpha=[1,0.9,0.9], line_width=2)
